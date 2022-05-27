e657762b959eb5bd0f04b5e4ed4ca2a71fab3219
Kevin Pearson <kevin_pearson@cable.comcast.com>
2022-05-27 10:21:16 -0700

    fix: When setting boolean properties to false in mock transport, getter is always returning default

--- src/js/shared/Prop/MockProps.mjs
+++ src/js/shared/Prop/MockProps.mjs
@@ -6,7 +6,7 @@ function mock(module, method, args, def) {
   const fullMethod = `${module}.${method}`
   if ((args == null) || (args.length === 0)) {
     // get
-    const rv = mocks[fullMethod] && mocks[fullMethod].value ? mocks[fullMethod].value : def
+    const rv = mocks[fullMethod] && (mocks[fullMethod].value != null) ? mocks[fullMethod].value : def
     return rv
   } else {
     // set
