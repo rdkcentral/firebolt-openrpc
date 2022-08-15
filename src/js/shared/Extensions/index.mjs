e7843303f7bb3912a29ef1fd27c08967c2c2771f
LaCivita, Jeremy <jeremy.lacivita@comcast.com>
2022-08-15 16:29:39 -0400

    Remove bespoke Core code from Extensions

--- src/js/shared/Extensions/index.mjs
+++ src/js/shared/Extensions/index.mjs
@@ -16,42 +16,8 @@ window.__firebolt.registerExtensionSDK = (id, initializer) => {
     }
 }
 
-// Method for handing off platform tokens to extension SDKs
 registerAPI('token', _ => Transport.send('authentication', 'token', { type: 'platform' }))
-
-registerAPI('authorize', (grants) => {
-    return new Promise( (resolve, reject) => {
-        // this will fail until we support capabilities
-        // once it works, this will trigger user grant UIs, and update the FAT
-        Transport.send('capabilities', 'request', { grants })
-        .then(granted => {
-            if (granted && granted.length) {
-                resolve(granted)
-            }
-            else {
-                reject()
-            }
-        })
-        // This is temporary. Will be handled by a user grant policy in future
-        .catch(_ => {
-            // assume all commerce capabilities require a pin prompt
-            if (grants.find(g => g.capability.startsWith('xrn:firebolt:capabilities:commerce:'))) {
-                Transport.send('profile', 'approvePurchase', {})
-                    .then(result => {
-                        if (result) {
-                            resolve(grants)
-                        }
-                        else {
-                            reject()
-                        }
-                    })
-                    .catch(error => {
-                        reject(error)
-                    })
-            }
-        })
-    })
-})
+registerAPI('authorize', grants => Transport.send('capabilities', 'request', {grants} ))
 
 function initialize(id, config) {
     if (!frozen) {
@@ -72,7 +38,7 @@ function active() {
     return Object.freeze(initialized.contact())
 }
 
-function registerAPI(name, method) {
+export function registerAPI(name, method) {
     apis[name] = method
 }
 
