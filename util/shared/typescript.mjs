b36f84c159559d4348662a3c33c07ce5b660c07f
Chakraborty <s.ei.chakraborty@accenture.com>
2022-10-27 14:49:43 +0530

    Worked on code review

--- util/shared/typescript.mjs
+++ util/shared/typescript.mjs
@@ -387,7 +387,7 @@ function getSchemaShape(moduleJson = {}, json = {}, schemas = {}, name = '', opt
       else {
         // grab the type for the non-array schema, so we get the link for free
         // added asPath and baseUrl to generate proper URL
-        let type = getSchemaType(module, json.items, schemas, {code: options.code, link: options.link, title: options.title, asPath: options.asPath, baseUrl: options.baseUrl})
+        let type = getSchemaType(module, json.items, schemas, options)
         // insert the [] into the type
         if (options.link) {
           type = type.replace(/\[(`?)(.*)(`?)\]/, '\[$1$2\[\]$3\]')
