b60fef976ff11bde54a44e36df3e729abd78b4b8
HaseenaSainul <h.sainul@metrological.com>
2023-05-31 10:29:50 -0400

    enum templates for native code added

---
+++ languages/c/templates/types/enum.cpp
@@ -0,0 +1,4 @@
+    /* ${title} ${description} */
+    ENUM_CONVERSION_BEGIN(${info.title}_${name})
+        { ${info.TITLE}_${NAME}_${key}, _T("${value}") },
+    ENUM_CONVERSION_END(${info.title}_${name})
