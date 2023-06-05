358e7fcb113cd5d5b543af467b0648bdba6f0f49
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2023-06-05 21:54:53 +0530

    Types and Accessor related fixes (#94)
    Types and Accessor related fixes:
    1. Accessor creation
    2. JsonContainer creation
    3. Types order placed based on depenedencies
    4. filtered redundant new lines
    5. UnamedSchema issue fixes
    6. Types and Accessors:  generation added for objects inside methods result/params
    7. AnyOf support added

--- languages/c/src/types/JSONHelpers.mjs
+++ languages/c/src/types/JSONHelpers.mjs
@@ -1,31 +1,54 @@
 const capitalize = str => str[0].toUpperCase() + str.substr(1)
+const getSdkNameSpace = () => 'FireboltSDK'
+const getJsonDataPrefix = () => 'JsonData_'
+const wpeJsonNameSpace = () => 'WPEFramework::Core::JSON'
 
-function getJsonContainerDefinition (name, props) {
-    name = capitalize(name)
-    let c = `    class ${name}: public Core::JSON::Container {
-          public:
-              ${name}(const ${name}&) = delete;
-              ${name}& operator=(const ${name}&) = delete;
-              ~${name}() override = default;
+const getJsonDataStructName = (modName, name, prefix = '') => {
+  let result =((prefix.length > 0) && (!name.startsWith(prefix)))  ? `${capitalize(modName)}::${getJsonDataPrefix()}${capitalize(prefix)}_${capitalize(name)}` : `${capitalize(modName)}::${getJsonDataPrefix()}${capitalize(name)}`
+
+  return ((result.includes(wpeJsonNameSpace()) === true) ? result : `${getSdkNameSpace()}::${result}`)
+}
+
+function getJsonContainerDefinition (schema, name, props) {
+  let c = schema.description ? ('    /*\n     * ${info.title} - ' + `${schema.description}\n     */\n`) : ''
+  name = getJsonDataPrefix() + capitalize(name)
+  c += `    class ${name}: public WPEFramework::Core::JSON::Container {
+    public:
+        ~${name}() override = default;
   
-          public:
-              ${name}()
-                  : Core::JSON::Container()
-              {`
+    public:
+        ${name}()
+            : WPEFramework::Core::JSON::Container()
+        {`
   
-      props.forEach(prop => {
-          c += `\n                Add(_T("${prop.name}"), &${capitalize(prop.name)});`
-      })
+  props.forEach(prop => {
+    c += `\n            Add(_T("${prop.name}"), &${capitalize(prop.name)});`
+  })
+  c += `\n        }\n`
+  c += `\n        ${name}(const ${name}& copy)
+        {`
+  props.forEach(prop => {
+    c += `\n            Add(_T("${prop.name}"), &${capitalize(prop.name)});`
+    c += `\n            ${capitalize(prop.name)} = copy.${capitalize(prop.name)};`
+  })
+  c += `
+        }\n
+        ${name}& operator=(const ${name}& rhs)
+        {`
+  props.forEach(prop => {
+    c += `\n            ${capitalize(prop.name)} = rhs.${capitalize(prop.name)};`
+  })
+  c += `\n            return (*this);
+        }\n
+    public:`
   
-      c += `\n            }\n\n       public:`
+  props.forEach(prop => {
+    c += `\n        ${prop.type} ${capitalize(prop.name)};`
+  })
   
-      props.forEach(prop => {
-          c += `\n            ${prop.type} ${capitalize(prop.name)};`
-      })
-  
-      c += '\n    };'
-      return c
-  }
+  c += '\n    };'
+  return c
+}
 
 export {
     getJsonContainerDefinition
