d3ef7f36878493734af447dfb1c46f451e1ede84
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2023-07-10 10:12:50 +0530

    polymorphic-pull: method and event implementation added (#107)

---
+++ languages/c/templates/declarations/polymorphic-pull-event.c
@@ -0,0 +1,4 @@
+/* ${method.name} - ${method.description} */
+typedef void* (*${info.Title}${method.Name}Callback)( const void* userData, ${method.pulls.param.type} );
+uint32_t ${info.Title}_Register_${method.Name}( ${info.Title}${method.Name}Callback userCB, const void* userData );
+uint32_t ${info.Title}_Unregister_${method.Name}( ${info.Title}${method.Name}Callback userCB);
