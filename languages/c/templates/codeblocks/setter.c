06f0f39fcd679441522f9670a4a3f07f2657eefa
HaseenaSainul <41037131+HaseenaSainul@users.noreply.github.com>
2023-07-17 21:13:35 +0530

    Generate polymorphci methods for anyOf param/result case (#110)

--- languages/c/templates/codeblocks/setter.c
+++ languages/c/templates/codeblocks/setter.c
@@ -1,7 +1,7 @@
-/* ${method.name} - ${method.description} */
+/* ${method.rpc.name} - ${method.description} */
 uint32_t ${info.Title}_${method.Name}( ${method.signature.params} )
 {
-    const string method = _T("${info.title}.${method.name}");
+    const string method = _T("${info.title.lowercase}.${method.rpc.name}");
 ${if.params}${method.params.serialization}${end.if.params}
     return FireboltSDK::Properties::Set(method, jsonParameters);
 }
