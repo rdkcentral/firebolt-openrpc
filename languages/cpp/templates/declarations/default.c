2c4592c439c797a1573d1a4c3e3ae4b5efcfe423
HaseenaSainul <h.sainul@metrological.com>
2023-09-06 01:23:57 -0400

    engine: add result.nonvoid flag to handle nonvoid result alone

--- languages/cpp/templates/declarations/default.c
+++ languages/cpp/templates/declarations/default.c
@@ -3,4 +3,4 @@
     ${method.description}
     ${method.params.annotations}${if.deprecated} * @deprecated ${method.deprecation}${end.if.deprecated}
     */
-    virtual int32_t ${method.Name}( ${method.signature.params}${if.result}${if.params}, ${end.if.params}${method.result.type}& ${method.result.name}${end.if.result} ) = 0;
+    virtual int32_t ${method.Name}( ${method.signature.params}${if.result.nonvoid}${if.params}, ${end.if.params}${method.result.type}& ${method.result.name}${end.if.result.nonvoid} ) = 0;
