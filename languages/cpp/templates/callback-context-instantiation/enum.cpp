6fd8d1150d6db93bc1955d28eb3bcdbca9e0496f
HaseenaSainul <h.sainul@metrological.com>
2023-11-07 11:35:27 -0500

    CPPSDK: define enum with cpp type

--- languages/cpp/templates/callback-context-instantiation/enum.cpp
+++ languages/cpp/templates/callback-context-instantiation/enum.cpp
@@ -1,3 +1,3 @@
                 if (strcmp(elements.Label(), "${property}") == 0) {
-                   ${property} = WPEFramework::Core::EnumerateType<${if.namespace.notsame}${info.Title}::${end.if.namespace.notsame}JsonData_${title}>(elements.Current().String().c_str(), false).Value();
+                   ${property} = WPEFramework::Core::EnumerateType<${if.namespace.notsame}${info.Title}::${end.if.namespace.notsame}${title}>(elements.Current().String().c_str(), false).Value();
                 }
