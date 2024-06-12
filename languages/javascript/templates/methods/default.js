b13d4cd581d1e990f8d01a3bba285df3b3bef7f7
Jeremy LaCivita <jeremy.lacivita@comcast.com>
2024-06-12 13:37:42 -0400

    fix: Transforms

--- languages/javascript/templates/methods/default.js
+++ languages/javascript/templates/methods/default.js
@@ -1,4 +1,4 @@
 
 function ${method.name}(${method.params.list}) {
-  return Gateway.request('${info.title}.${method.name}', { ${method.params.list} })
+  return Gateway.request('${info.title}.${method.name}', { ${method.params.list} })${method.transform}
 }
