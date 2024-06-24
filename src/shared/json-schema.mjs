5d4205e58afbb9a221443a6b266a5bb39930627b
Shah, Kevin <Kevin_Shah@comcast.com>
2024-06-23 21:06:43 -0400

    chore: test pr

--- src/shared/json-schema.mjs
+++ src/shared/json-schema.mjs
@@ -426,19 +426,12 @@ function mergeOneOf(schema) {
   return union(schema.oneOf)
 }
 
-const getSafeEnumKeyName = function(value, keyPrefix = '') {
-  if (keyPrefix != '') {
-    value = keyPrefix + '_' + value
-  }
-
-  let key = value.split(':').pop()          // use last portion of urn:style:values
-    .replace(/\+/g, '_plus')                // change + to _plus
-    .replace(/[\.\-\/\;]/g, '_')            // replace special characters
-    .replace(/([a-z])([A-Z0-9])/g, '$1_$2') // camel -> snake case
-    .replace(/^([0-9]+\_([0-9]+)?)/, 'v$1') // insert `v` in front of things that look like version numbers
-
-  return key.toUpperCase()
-}
+const getSafeEnumKeyName = (value) => value.split(':').pop()                           // use last portion of urn:style:values
+                                        .replace(/[\.\-]/g, '_')                       // replace dots and dashes
+                                        .replace(/\+/g, '_plus')                       // change + to _plus
+                                        .replace(/([a-z])([A-Z0-9])/g, '$1_$2')        // camel -> snake case
+                                        .replace(/^([0-9]+(\.[0-9]+)?)/, 'v$1')        // insert `v` in front of things that look like version numbers
+                                        .toUpperCase()
 
 export {
   getSchemaConstraints,
