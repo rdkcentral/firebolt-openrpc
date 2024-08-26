4a734d8d64efbdbb080a024b5b3cefe1a8b7f6ad
kschri201_comcast <kurt_schriefer@cable.comcast.com>
2024-08-26 11:40:22 -0400

    fix: Property.cpp

--- languages/cpp/templates/result-instantiation/property.cpp
+++ languages/cpp/templates/result-instantiation/property.cpp
@@ -1,3 +1,3 @@
 ${shape}            ${if.non.anyOf}${if.non.array}${if.non.object}${if.optional}if (jsonResult.${Property}.IsSet()) {
                 ${base.title}Result${level}.${property} = jsonResult.get("${Property}").${additional.type};
-            }${end.if.optional}${if.non.optional}${base.title}Result${level}.${property} = jsonResult.${Property}.Value();${end.if.non.optional}${end.if.non.object}${end.if.non.array}${end.if.non.anyOf}
+            }${end.if.optional}${if.non.optional}${base.title}Result${level}.${property} = jsonResult.get("${property.raw}").${additional.type};${end.if.non.optional}${end.if.non.object}${end.if.non.array}${end.if.non.anyOf}
