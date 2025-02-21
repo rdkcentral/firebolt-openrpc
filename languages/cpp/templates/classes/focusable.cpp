f1006fc9df1341a7d6cfdec5a041920a80a420f1
Tomasz Blaszczak <tomaszblaszczak.w2@outlook.com>
2025-02-21 11:15:07 +0100

    feat: Update templates used by provider to match with requirements for bi-directional

--- languages/cpp/templates/classes/focusable.cpp
+++ languages/cpp/templates/classes/focusable.cpp
@@ -1,44 +1,18 @@
-    class ${info.Title}${method.Name}Session : virtual public I${info.Title}Session {
-    public:
-        ${info.Title}${method.Name}Session( const std::string& correlationId )
-        : _correlationId(correlationId)
-        {
-        }
-
-        std::string correlationId() const override
-        {
-            return _correlationId;
-        }
-        void focus( Firebolt::Error *err = nullptr ) override
-        {
-            ProviderFocusSession("${info.title.lowercase}.${method.name}Focus", _correlationId, err);
-        }
-        void result( ${provider.xresponse.name} response, Firebolt::Error *err = nullptr ) override
-        {
-            ProviderResultSession("${info.title.lowercase}.${method.name}Response", _correlationId, response, err);
-        }
-        void error( ${provider.xerror.name} error, Firebolt::Error *err = nullptr ) override
-        {
-            ProviderErrorSession("${info.title.lowercase}.${method.name}Error", _correlationId, error, err);
-        }
-
-    public:
-        std::string _correlationId;
-    };
-    static void ${info.Title}${method.Name}SessionInnerCallback( void* provider, const void* userData, void* jsonResponse )
+    static std::string ${info.Title}${method.Name}SessionInnerCallback( void* provider, void* jsonRequest )
     {
-${event.callback.serialization}
-        ASSERT(proxyResponse.IsValid() == true);
+        WPEFramework::Core::ProxyType<JsonData_${info.Title}ProviderRequest>& proxyRequest = *(reinterpret_cast<WPEFramework::Core::ProxyType<JsonData_${info.Title}ProviderRequest>*>(jsonRequest));
 
-        if (proxyResponse.IsValid() == true) {
-${event.callback.initialization}
+        ASSERT(proxyRequest.IsValid() == true);
 
-${event.callback.instantiation}
-            proxyResponse.Release();
-
-            std::unique_ptr<I${info.Title}Session> ${info.title.lowercase}${method.Name}Session = std::make_unique<${info.Title}${method.Name}Session>(${method.result.name}.correlationId);
-            I${info.Title}Provider& ${info.title.lowercase}Provider = *(reinterpret_cast<I${info.Title}Provider*>(provider));
-            ${info.title.lowercase}Provider.${method.name}(${method.result.name}.parameters, std::move(${info.title.lowercase}${method.Name}Session));
+        if (!proxyRequest.IsValid()) {
+            return R"({"error": { "code": )" + std::to_string(static_cast<int32_t>(Firebolt::Error::InvalidParams)) + R"(, "message": "Invalid Parameters"}, "result": ""})";
         }
+        std::string requestMessage = (*proxyRequest).Parameters.Message;
+
+        proxyRequest.Release();
+
+        I${info.Title}Provider& ${info.title.lowercase}Provider = *(reinterpret_cast<I${info.Title}Provider*>(provider));
+        std::string result = ${info.title.lowercase}Provider.${method.name}(requestMessage.parameters);
+        return "{\"result\":\"" + result + "\"}";
     }
 
--- languages/cpp/templates/interfaces/focusable.h
+++ languages/cpp/templates/interfaces/focusable.h
@@ -1,1 +1,1 @@
-    virtual void ${method.name}( ${method.signature.params}, std::unique_ptr<I${info.Title}Session> session ) = 0;
+    virtual void ${method.name}( ${method.signature.params} ) = 0;
